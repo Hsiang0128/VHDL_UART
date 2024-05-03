e6408081c909559a68175263db31cc3d16d63ba0
Li Hsiang Huang <100186660+Hsiang0128@users.noreply.github.com>
2024-05-04 06:14:51 +0800

    Baud generator finish

--- BaudRate_generator.vhd
+++ BaudRate_generator.vhd
@@ -1,16 +1,31 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
 entity BaudRate_generator is
+	generic(
+		CLK_INPUT 	: integer :=  40000000;
+		BAUD_RATE	: integer :=	19200
+	);
 	port(
 		clk	: in std_logic;
-		tick	: in std_logic
+		tick	: out std_logic
 	);
 end BaudRate_generator;
-
-architecture Behavioral of BaudRate_generator is
-	
+architecture behavior of BaudRate_generator is
+	signal tmp : std_logic := '0';
+	signal counter : integer := 0;
 begin
-
-
-end Behavioral;
+	process(clk)begin
+		if(clk = '1')then
+			tick <= tmp;
+			if(counter = (CLK_INPUT/BAUD_RATE)/2)then
+				 tmp	<= not tmp;
+				counter <= 0;
+			else
+				counter <= counter + 1; 
+			end if;
+		end if;
+	end process;
+end behavior;
 
---
+++ BaudRate_generator_tb.vhd
@@ -0,0 +1,39 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY BaudRate_generator_tb IS
+END BaudRate_generator_tb;
+
+ARCHITECTURE behavior OF BaudRate_generator_tb IS
+
+	COMPONENT BaudRate_generator
+	port(
+		clk	: in std_logic;
+		tick	: out std_logic
+	);
+	END COMPONENT;
+
+	SIGNAL clk	: std_logic;
+	SIGNAL tick	: std_logic;
+BEGIN
+	uut: entity work.BaudRate_generator(behavior)
+
+		GENERIC MAP(
+			CLK_INPUT	=> 500,
+ 			BAUD_RATE	=> 10
+		)
+		PORT MAP(
+			clk => clk,
+			tick => tick
+		);
+  tb : PROCESS
+  BEGIN
+	  clk <= '0';
+	  wait for 1ns;
+	  clk <= '1';
+	  wait for 1ns;
+  END PROCESS tb;
+--  End Test Bench 
+
+END behavior;
