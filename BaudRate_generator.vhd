be0fdd862f9454ecc3d1cf200e228e58f1c98393
Li Hsiang Huang <100186660+Hsiang0128@users.noreply.github.com>
2024-05-04 05:33:59 +0800

    add BaudRate_generator

---
+++ BaudRate_generator.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+entity BaudRate_generator is
+	port(
+		clk	: in std_logic;
+		tick	: in std_logic
+	);
+end BaudRate_generator;
+
+architecture Behavioral of BaudRate_generator is
+	
+begin
+
+
+end Behavioral;
+
