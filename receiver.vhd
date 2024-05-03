8ec00a12d51ce64acaae280bf6b3d8afe1368583
Li Hsiang Huang <100186660+Hsiang0128@users.noreply.github.com>
2024-05-03 16:11:33 +0800

    add uart_rx_basic

---
+++ receiver.vhd
@@ -0,0 +1,97 @@
+library ieee ;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity uart_rx is
+	generic(
+		DBIT 		: integer := 8 ; 	-- # data bits
+		SB_TICK	: integer := 16 	-- #tick for stop bits
+	) ;
+	port(
+		clk 				: in std_logic;
+		reset				: in std_logic;
+		rx 				: in std_logic;
+		s_tick 			: in std_logic;
+		rx_done_tick	: out std_logic;
+		dout 				: out std_logic_vector( 7 downto 0 )
+	);
+end uart_rx;
+architecture code of uart_rx is
+	type state_type is (
+		IDLE,
+		START,
+		DATA,
+		STOP
+	);
+	signal state_reg	: state_type;
+	signal state_next	: state_type;
+	signal s_reg		: unsigned(3 downto 0);
+	signal s_next		: unsigned(3 downto 0);
+	signal n_reg		: unsigned(2 downto 0);
+	signal n_next 		: unsigned(2 downto 0);
+	signal b_reg		: std_logic_vector(7 downto 0);
+	signal b_next		: std_logic_vector(7 downto 0);
+begin
+
+	dout <= b_reg;
+	process(clk,reset) begin
+		if(reset = '1')then
+			state_reg <= IDLE ;
+			s_reg <= (others => '0');
+			n_reg <= (others => '0');
+			b_reg <= (others => '0');
+		elsif(clk'event and clk = '1')then
+			state_reg <= state_next ;
+			s_reg <= s_next ;
+			n_reg <= n_next ;
+			b_reg <= b_next ;
+		end if ;
+	end process;
+	
+	process(state_reg,s_reg,n_reg,b_reg,s_tick,rx) begin
+		state_next <= state_reg;
+		s_next <= s_reg;
+		n_next <= n_reg;
+		b_next <= b_reg;
+		rx_done_tick <= '0';
+		case state_reg is
+			when IDLE =>
+				if(rx = '0')then
+					state_next 	<= START;
+					s_next 		<= (others => '0');
+				end if;
+			when START =>
+				if(s_tick = '1')then
+					if(s_reg = 7) then
+						state_next <= DATA;
+						s_next <= (others => '0');
+						n_next <= (others => '0');
+					else
+						s_next <= s_reg + 1;
+					end if;
+				end if;
+			when DATA =>
+				if(s_tick = '1')then
+					if(s_reg = 15)then
+						s_next <= (others => '0');
+						b_next <= rx & b_reg(7 downto 1);
+						if(n_reg = (DBIT-1))then
+							state_next <= STOP;
+						else 
+							n_next <= n_reg + 1;
+						end if;
+					else 
+						s_next <= s_reg + 1;
+					end if;
+				end if;
+			when STOP =>
+				if(s_tick = '1') then
+					if(s_reg = SB_TICK - 1) then
+						state_next <= IDLE;
+						rx_done_tick <= '1';
+					else
+						s_next <= s_reg + 1;
+					end if;
+				end if;
+		end case ;
+	end process;
+end code;
---
+++ uart_rx_tb.vhd
@@ -0,0 +1,75 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY uart_rx_tb IS
+END uart_rx_tb;
+ 
+ARCHITECTURE behavior OF uart_rx_tb IS 
+	COMPONENT uart_rx
+	PORT(
+		clk : IN  std_logic;
+		reset : IN  std_logic;
+		rx : IN  std_logic;
+		s_tick : IN  std_logic;
+		rx_done_tick : OUT  std_logic;
+		dout : OUT  std_logic_vector(7 downto 0)
+	  );
+	END COMPONENT;
+	
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '1';
+   signal rx : std_logic := '1';
+   signal s_tick : std_logic := '0';
+   signal rx_done_tick : std_logic;
+   signal dout : std_logic_vector(7 downto 0);
+	
+	constant testData			: std_logic_vector(7 downto 0) := "11001001"; -- my test data
+   constant clk_period 		: time := 2 ns;
+	constant s_tick_period 	: time := 64 ns;
+ 
+BEGIN
+
+   uut: uart_rx PORT MAP (
+          clk => clk,
+          reset => reset,
+          rx => rx,
+          s_tick => s_tick,
+          rx_done_tick => rx_done_tick,
+          dout => dout
+        );
+
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+	
+	s_tick_process :process
+   begin
+		s_tick <= '0';
+		wait for s_tick_period/2;
+		s_tick <= '1';
+		wait for s_tick_period/2;
+   end process;
+	
+   stim_proc: process
+   begin	
+		wait for 10ns;
+		reset <= '0';
+		wait for 10ns;
+		----<Start>-----------------------------
+		rx <= '0'; 
+		wait for s_tick_period;
+		----<Send Data>-------------------------
+		for i in 0 to 7 loop
+			rx <= testData(i);
+			wait for s_tick_period;
+		end loop;
+		----<End>-----------------------------
+		rx <= '1';
+		wait for 64ns;
+      wait;
+   end process;
+END;
